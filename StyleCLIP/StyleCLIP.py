93da2d93652afc57f3299c8849b2d9c8662b6fe8
azf99 <azfarlari1999@gmail.com>
2021-07-02 21:04:05 +0530

    Debugged StyleCLIP

--- StyleCLIP/StyleCLIP.py
+++ StyleCLIP/StyleCLIP.py
@@ -10,7 +10,9 @@ from PIL import Image
 from StyleCLIP.stylegan_models import g_all, g_synthesis, g_mapping
 from StyleCLIP.utils import GetFeatureMaps, transform_img, compute_loss
 
-torch.manual_seed(20)
+#torch.manual_seed(20)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
 vgg16 = torchvision.models.vgg16(pretrained=True).to(device)
@@ -29,57 +31,57 @@ g_synthesis.eval()
 g_synthesis.to(device)
 
 class StyleCLIP(object):
-    def __init__(self, output_path = './generations', prompt = ""):
-    self.output_path = output_path
-    self.batch_size = 1
-    self.prompt = prompt
-    self.lr = 1e-2
-    self.img_save_freq = 10
-    self.ref_img_path = ''
+    def __init__(self, output_path = './generations', prompt: str = "", img_path = None):
+      self.output_path = output_path
+      self.batch_size = 1
+      self.prompt = prompt
+      self.lr = 1e-2
+      self.img_save_freq = 10
+      self.ref_img_path = img_path
 
-    self.output_dir = os.path.join(output_path, f'"{prompt}"')
+      self.output_dir = os.path.join(output_path, f'"{prompt}"')
 
-    self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    #print("USING ", self.device)
+      self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+      #print("USING ", self.device)
 
-    if not os.path.exists(self.output_dir):
-        os.makedirs(self.output_dir)
+      if not os.path.exists(self.output_dir):
+          os.makedirs(self.output_dir)
 
-    self.latent_shape = (batch_size, 1, 512)
+      self.latent_shape = (self.batch_size, 1, 512)
 
-    self.normal_generator = torch.distributions.normal.Normal(
-        torch.tensor([0.0]),
-        torch.tensor([1.]),
-    )
+      self.normal_generator = torch.distributions.normal.Normal(
+          torch.tensor([0.0]),
+          torch.tensor([1.]),
+      )
 
-    # init_latents = normal_generator.sample(latent_shape).squeeze(-1).to(device)
-    self.latents_init = torch.zeros(latent_shape).squeeze(-1).to(device)
-    self.latents = torch.nn.Parameter(self.latents_init, requires_grad=True)
+      # init_latents = normal_generator.sample(latent_shape).squeeze(-1).to(device)
+      self.latents_init = torch.zeros(self.latent_shape).squeeze(-1).to(self.device)
+      self.latents = torch.nn.Parameter(self.latents_init, requires_grad=True)
 
-    self.optimizer = torch.optim.Adam(
-        params=[self.latents],
-        lr=self.lr,
-        betas=(0.9, 0.999),
-    )
+      self.optimizer = torch.optim.Adam(
+          params=[self.latents],
+          lr=self.lr,
+          betas=(0.9, 0.999),
+      )
 
-    self.clip_transform = torchvision.transforms.Compose([
-    # clip_preprocess.transforms[2],
-    clip_preprocess.transforms[4],
-    ])
+      self.clip_transform = torchvision.transforms.Compose([
+      # clip_preprocess.transforms[2],
+      clip_preprocess.transforms[4],
+      ])
 
-    if self.ref_img_path is None:
-        self.ref_img = None
-    else:
-        self.ref_img = clip_preprocess(Image.open(self.ref_img_path)).unsqueeze(0).to(self.device)
+      if self.ref_img_path is None:
+          self.ref_img = None
+      else:
+          self.ref_img = clip_preprocess(Image.open(self.ref_img_path)).unsqueeze(0).to(self.device)
 
-    self.clip_normalize = torchvision.transforms.Normalize(
-        mean=(0.48145466, 0.4578275, 0.40821073),
-        std=(0.26862954, 0.26130258, 0.27577711),
-    )
+      self.clip_normalize = torchvision.transforms.Normalize(
+          mean=(0.48145466, 0.4578275, 0.40821073),
+          std=(0.26862954, 0.26130258, 0.27577711),
+      )
 
 
     def truncation(self, x, threshold=0.7, max_layer=8):
-        avg_latent = torch.zeros(batch_size, x.size(1), 512).to(self.device)
+        avg_latent = torch.zeros(self.batch_size, x.size(1), 512).to(self.device)
         interp = torch.lerp(avg_latent, x, threshold)
         do_trunc = (torch.arange(x.size(1)) < max_layer).view(1, -1, 1).to(self.device)
         return torch.where(do_trunc, interp, x)
@@ -93,7 +95,7 @@ class StyleCLIP(object):
     def compute_clip_loss(self, img, text):
         # img = clip_transform(img)
         img = torch.nn.functional.upsample_bilinear(img, (224, 224))
-        tokenized_text = clip.tokenize([text]).to(device)
+        tokenized_text = clip.tokenize([text]).to(self.device)
 
         img_logits, _text_logits = clip_model(img, tokenized_text)
 
@@ -121,7 +123,7 @@ class StyleCLIP(object):
     def run(self):
         counter = 0
         path_to_res = ""
-        while True:
+        while counter <= self.img_save_freq:
             dlatents = self.latents.repeat(1,18,1)
             img = g_synthesis(dlatents)
             
@@ -141,13 +143,11 @@ class StyleCLIP(object):
             self.optimizer.step()
 
             if counter % self.img_save_freq == 0:
-                img = tensor_to_pil_img(img)
-                img.save(os.path.join(output_dir, f'{counter}.png'))
-                path_to_res = os.path.join(output_dir, f'{counter}.png')
+                img = self.tensor_to_pil_img(img)
+                img.save(os.path.join(self.output_dir, f'{counter}.png'))
+                path_to_res = os.path.join(self.output_dir, f'{counter}.png')
 
                 print(f'Step {counter}')
                 print(f'Loss {loss.data.cpu().numpy()[0][0]}')
-                break
-
             counter += 1
         return path_to_res
--- app.py
+++ app.py
@@ -2,10 +2,24 @@ from StyleCLIP.StyleCLIP import *
 from LatentRevisions import *
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import datetime
+import base64
 
 app = Flask(__name__)
 
 CORS(app)
+
+UPLOAD_FOLDER = "UPLOAD_FOLDER/"
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+def save_img(label, file):
+    current_time = str(datetime.datetime.now()).replace('-', '_').replace(':', '_')
+    filename = UPLOAD_FOLDER + label + current_time + ".png"
+    file.save(filename)
+    return(filename)
 
 @app.route('/index', methods = ["GET", "POST"])
 def hello_world():
@@ -13,19 +27,30 @@ def hello_world():
 
 @app.route('/styleclip', methods = ["POST"])
 def stype_clip():
-    #prompt = request.form["prompt"]
-    prompt = 'An image with the face of a blonde woman with blonde hair and purple eyes'
+    prompt = request.form["prompt"]
+    # check if reference image is given
+    try:
+        img = request.files["upload_file"]
+        filename = save_img("styleclip", file)
+    except Exception as e:
+        print(e)
+    #prompt = 'An image with the face of a blonde woman with blonde hair and purple eyes'
     model = StyleCLIP(prompt = prompt)
     path = model.run()
+
+    res = open(path, "rb")
+    encoded_image = base64.b64encode(res.read())
+
+    # return the generated image
     return jsonify({
         "status": "success",
-        "image": path
+        "image": encoded_image
     })
 
 @app.route('/latent_revision', methods = ["POST"])
 def latent_revisions():
-    #prompt = request.form["prompt"]
-    prompt = 'A beautiful person'
+    prompt = request.form["prompt"]
+    #prompt = 'A beautiful person'
     model = LatentRevisions(prompt = prompt)
     path = model.run()
     return jsonify({
