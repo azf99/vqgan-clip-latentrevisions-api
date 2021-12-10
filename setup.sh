769de4af442c38394ea455d816c43324f9f30a3f
azf99 <azfarlari1999@gmail.com>
2021-12-11 01:58:50 +0530

    Revert "switched to local backup dependencies"
    This reverts commit a0f2c6b6055f4b7d1a9b3d312ba47867caeb813f.

--- setup.sh
+++ setup.sh
@@ -4,23 +4,23 @@ mkdir -p "./logs/vqgan_imagenet_f16_16384/checkpoints"
 mkdir -p "./logs/vqgan_imagenet_f16_16384/configs"
 
 if [ -z "$(ls -A ./logs/vqgan_imagenet_f16_16384/checkpoints/)" ]; then
-  wget 'https://mead-model-dependencies.s3.amazonaws.com/last.ckpt' -O './logs/vqgan_imagenet_f16_16384/checkpoints/last.ckpt' 
-  wget 'https://mead-model-dependencies.s3.amazonaws.com/model.yaml' -O './logs/vqgan_imagenet_f16_16384/configs/model.yaml' 
+  wget 'https://heibox.uni-heidelberg.de/f/867b05fc8c4841768640/?dl=1' -O './logs/vqgan_imagenet_f16_16384/checkpoints/last.ckpt' 
+  wget 'https://heibox.uni-heidelberg.de/f/274fb24ed38341bfa753/?dl=1' -O './logs/vqgan_imagenet_f16_16384/configs/model.yaml' 
 fi
 echo "VQGAN weights saved...."
 
-wget https://mead-model-dependencies.s3.amazonaws.com/karras2019stylegan-ffhq-1024x1024.for_g_all.pt
+wget https://github.com/lernapparat/lernapparat/releases/download/v2019-02-01/karras2019stylegan-ffhq-1024x1024.for_g_all.pt
 
 #sudo add-apt-repository ppa:redislabs/redis
 sudo apt update
 sudo apt-get install -y redis
 
-pip3 install git+https://github.com/bakztfuture/CLIP.git
+pip3 install git+https://github.com/openai/CLIP.git
 pip3 install -r requirements.txt
 pip3 uninstall torchtext --yes
 
 #cd /content/
-git clone https://github.com/bakztfuture/taming-transformers  
+git clone https://github.com/CompVis/taming-transformers  
 cd taming-transformers
 
 # !cp /content/drive/MyDrive/vqgan_imagenet_f16_16384-20210325T002625Z-001.zip /content/vq.zip
